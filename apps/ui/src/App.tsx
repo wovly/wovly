1d5843634eeb56a382088f487b5627e0611cfc66
Jeffrey Chou <jeffchou@Jeffreys-MacBook-Pro-2.local>
2026-02-03 09:00:18 -0500

    Update login privacy note wording

--- apps/ui/src/App.tsx
+++ apps/ui/src/App.tsx
@@ -5826,7 +5826,7 @@ function LoginPage({ onLogin }: { onLogin: (user: AuthUser) => void }) {
             <rect x="3" y="11" width="18" height="11" rx="2" ry="2"/>
             <path d="M7 11V7a5 5 0 0 1 10 0v4"/>
           </svg>
-          <span>All data is stored locally on your device. Nothing is sent to the cloud.</span>
+          <span>All login data is stored locally on your device.</span>
         </div>
       </div>
     </div>
